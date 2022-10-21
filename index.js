97f1be882bcbd70e86f5ccbae956275072f2c8ee
ZiUioM <116329149+ZiUioM@users.noreply.github.com>
2022-10-20 22:45:46 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const yesBtn = document.querySelector('#yesBtn');
 
 yesBtn.addEventListener('click',function () {
-    alert('Sabia que ibas a decir que si uwu')
+    alert('Sabia que dirías que si jsjs')
 });
 
 const noBtn = document.querySelector('#noBtn');
